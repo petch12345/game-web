b11b4858bf50905ab3d8d1a92885f30bc71c61e8
petch12345 <piyapatmanit2550@gmail.com>
2025-09-25 14:57:17 +0700

    Add files via upload

--- index.js
+++ index.js
@@ -1,5 +1,3 @@
-
-
 function playgame() {
     userInput = document.getElementById("user-Input").value;
     console.log(userInput);
@@ -7,28 +5,39 @@ function playgame() {
     botinput = Math.floor(Math.random() * 100) % 3;
     console.log(botinput);
 
-    if (userInput >= 3 && userInput < 0 ) {
-        console.log("false input");
+    if (userInput >= 3 ) {
+        status = `<span>False input</span>`;
     }
 
+    else if (userInput === "") {
+        status = `<span>False input</span>`;
+    } 
+
+    else if (userInput < 0) {
+        status = `<span>False input</span>`;
+    } 
+
     else if (userInput == botinput) {
-        console.log("Draw");
+        status = `<span>Drawn</span>`;
     } 
 
     else if (userInput == 0 && botinput == 2) {
-        console.log("User Wins");
+        status = `<span>User Wins</span>`;
     }
 
     
     else if (userInput == 1 && botinput == 0) {
-        console.log("User Wins");
+        status = `<span>User Wins</span>`;
     } 
     
     else if (userInput == 2 && botinput == 1) {
-        console.log("User Wins");
+        status = `<span>User Wins</span>`;
     } 
     else {
-        console.log("Bot Wins");
+        status = `<span>Bot Wins</span>`;
     }
+    document.getElementById('result').innerHTML = `Total is: <b>${status}</b>`;
+    document.getElementById('bot').innerHTML = `Bot is: <b>${botinput}</b>`;
+    ;
 }
 
